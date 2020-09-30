e0a73a6de05416d40036e1960f5ed507a6793238
Noah <mahinoah_2019@yahoo.com>
2020-09-30 01:46:18 -0700

    first commit

---
+++ src/LightSwitchObject.java
@@ -0,0 +1,11 @@
+public class LightSwitchObject {
+
+    public static void main(String[] args) {
+
+
+        LightSwitch mySwitch = new LightSwitch("NS001650", true);
+
+        mySwitch.setOn(false);
+        System.out.println(mySwitch.getIsOn());
+    }
+    }
